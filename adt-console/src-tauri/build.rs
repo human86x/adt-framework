acbac0644817d89b0d34ad7f790bffbbf7bea85a
human86x <skripachenko.evgeny@gmail.com>
2026-06-17 17:56:59 +0100

    [ADT] edit adt-console/src-tauri/build.rs - GEMINI (DevOps_Engineer)

--- adt-console/src-tauri/build.rs
+++ adt-console/src-tauri/build.rs
@@ -1,3 +1,12 @@
+use std::io::Write;
+use std::fs::File;
+use std::path::Path;
+
 fn main() {
+    let build_time = chrono::Utc::now().format("%Y-%m-%d %H:%M:%S UTC").to_string();
+    let dest_path = Path::new("../src/build_time.txt");
+    let mut f = File::create(&dest_path).unwrap();
+    f.write_all(build_time.as_bytes()).unwrap();
+
     tauri_build::build()
 }
